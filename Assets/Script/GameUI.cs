a5c8edf82d756ef2c9c24877bf6edb087a3e8d28
ghdrudtj <ghdqwer432@gmail.com>
2024-09-03 16:01:12 +0900

    방해: 도둑

--- Assets/Script/GameUI.cs
+++ Assets/Script/GameUI.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public class GameUI : MonoBehaviour
 {
     public GameObject MenuUI;
-
+    bool GameActive =false;
 
     private void Update()
     {
@@ -18,8 +18,16 @@ public class GameUI : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape)) 
         {
             MenuUI.SetActive(!MenuUI.activeSelf);
+            if (GameActive)
+            {
+                Time.timeScale = 1.0f;
+                GameActive = false;
+            }
+            else
+            {
+                Time.timeScale = 0;
+                GameActive = true;
+            }
         }
     }
-    
-
 }
---
+++ Assets/Script/NewBehaviourScript.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public static Disturbance disturbance;
+    public GameObject thiefObj;
+    public int thiefMaxNum = 10; // 도둑의 최대 클릭 수
+    public float spawnInterval = 5;  // 도둑 소환 간격
+    public float thiefActiveDuration = 5f; // 도둑 활성화 시간
+
+    private int currentThiefClicks;
+    private float spawnTimer;
+    private bool isThiefActive;
+
+    private bool Thiefclear;
+    private bool Thiefover;
+    void Start()
+    {
+        StartCoroutine(ThiefSpawnRoutine());
+    }
+
+    IEnumerator ThiefSpawnRoutine()
+    {
+        while (true) // 계속 반복하여 도둑을 일정 간격으로 소환
+        {
+            yield return new WaitForSecondsRealtime(spawnInterval);
+            if (!isThiefActive)
+            {
+                // 도둑 소환
+                SpawnThief();
+                isThiefActive = true;
+
+                // 도둑이 활성화되는 동안 대기
+                float currentTime = 0f;
+                while (currentTime < thiefActiveDuration)
+                {
+                    currentTime += Time.deltaTime;
+                    yield return null;
+                }
+            }
+            // 다음 도둑 소환을 위한 대기
+            if(currentThiefClicks <= thiefMaxNum)
+            {
+                OverThief();
+            }
+            else
+            {
+                EvaluateThief();
+            }
+            isThiefActive = false;
+            Thiefclear=false;
+            Thiefover=false;
+        }
+    }
+
+    void SpawnThief()
+    {
+        thiefObj.SetActive(true);
+        currentThiefClicks = 0; // 클릭 수 초기화
+        Debug.Log("도둑 소환!");
+    }
+
+    void EvaluateThief()
+    {
+        if (currentThiefClicks >= thiefMaxNum)
+        {
+            Debug.Log("도둑 성공!");
+            Thiefclear = true;
+        }
+       
+        thiefObj.SetActive(false);
+    }
+    void OverThief()
+    {
+        if (currentThiefClicks <= thiefMaxNum)
+        {
+            Debug.Log("도둑 실패!");
+            Coin.instance.coin -= Coin.instance.coin / 10;
+            Debug.Log("coin = " + Coin.instance.coin / 10);
+            Thiefover = true;
+        }
+        thiefObj.SetActive(false);
+    }
+
+    public void ThiefClick()
+    {
+        if (isThiefActive)
+        {
+            currentThiefClicks++;
+            Debug.Log("클릭 수 = " + currentThiefClicks);
+
+            // 클릭 수가 목표에 도달하면 도둑을 즉시 비활성화
+            if (currentThiefClicks >= thiefMaxNum)
+            {
+                EvaluateThief();
+            }
+            
+        }
+    }
+
+    private void Update()
+    {
+    }
+}
